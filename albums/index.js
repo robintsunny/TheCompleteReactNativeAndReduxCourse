6b7021bcd23bc4eb89fb12ca646c43e47309ced2
robin <robintsunny@gmail.com>
2018-06-26 13:16:56 -0700

    Begin User Auth

---
+++ albums/index.js
@@ -0,0 +1,21 @@
+// import library to help create a component
+import React from "react";
+import {
+    Text,
+    AppRegistry,
+    View
+} from "react-native";
+import Header from "./src/components/Header";
+import AlbumList from "./src/components/AlbumList";
+
+// create component
+const App = () => ( 
+    <View style = {{flex: 1}}>
+    <Header headerText = {'Albums!'}/> 
+        <AlbumList />
+    </View>
+)
+
+
+// render it to device
+AppRegistry.registerComponent("albums", () => App);
