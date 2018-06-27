8378f65d62d90a799ab5385b4d7d2a23629dc782
robin <robintsunny@gmail.com>
2018-06-27 09:31:40 -0700

    Add Loading Spinner

--- auth/src/components/LoginForm.js
+++ auth/src/components/LoginForm.js
@@ -1,26 +1,51 @@
 import React from 'react'
 import {Text} from 'react-native'
 
-import { Button, Card, CardSection,Input } from './common'
+import { Button, Card, CardSection,Input, Spinner} from './common'
 import firebase from 'firebase'
 
  
 
 class LoginForm extends React.Component {
-    state = {email: '', password:'', error:''}
+    state = {email: '', password:'', error:'', loading: false}
 
     onButtonPress() {
         const {email, password,error} = this.state;
         
-        this.setState({error:''})
+        this.setState({error:'', loading: true})
 
         firebase.auth().signInWithEmailAndPassword(email,password)
+            .then(this.onLoginSuccess.bind(this))
             .catch(() => {
                 firebase.auth().createUserWithEmailAndPassword(email,password)
-                .catch(() => {
-                    this.setState({error:'Authentication Failed'})
-                })
+                .then(this.onLoginSuccess.bind(this))
+                .catch(this.onLoginFail.bind(this))
             })
+    }
+
+    onLoginSuccess () {
+        this.setState({
+            email: '',
+            password: '',
+            loading: false,
+            error: ''
+        })
+    }
+
+    onLoginFail () {
+        this.setState({error: 'Authentication Failed', loading: false})
+    }
+
+    renderButton() {
+        if (this.state.loading) {
+            return <Spinner size='small' />
+        } else {
+            return (
+                <Button onPress = {this.onButtonPress.bind(this)}>
+                    Login
+                </Button>
+            )
+        }
     }
 
 
@@ -51,9 +76,7 @@ class LoginForm extends React.Component {
                 <Text style={styles.errorTextStyle}>{this.state.error}</Text>
 
                 <CardSection>
-                    <Button onPress={this.onButtonPress.bind(this)}>
-                        Login
-                    </Button>
+                    {this.renderButton()}
                 </CardSection>
 
             </Card>
