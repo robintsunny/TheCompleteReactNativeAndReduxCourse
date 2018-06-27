695c0e4b41b7f7d8a7d155e700bc284abb75a448
robin <robintsunny@gmail.com>
2018-06-26 17:21:40 -0700

    Add Auth Page

---
+++ auth/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import {View, Text} from 'react-native'
+import {Header} from './components/common'
+
+
+
+class App extends React.Component {
+    render() {
+        return (
+            <View>
+                <Header headerText="Authentication" />
+                <Text>An App</Text>
+            </View>
+        ) 
+    }
+}
+
+export default App;
