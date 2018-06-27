ef70c09cee36edf27def42fe9982b00158d88eb4
robin <robintsunny@gmail.com>
2018-06-27 10:04:40 -0700

    Log User Out

--- auth/src/App.js
+++ auth/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import {View } from 'react-native'
-import {Header} from './components/common'
+import {Header, Button, Spinner} from './components/common'
 import firebase from 'firebase'
 import LoginForm from './components/LoginForm';
 
 
 class App extends React.Component {
+    state = {loggedIn: null}
+
     componentWillMount() {
         firebase.initializeApp({
             apiKey: "AIzaSyCMloGtVWqOCmlP3NeXQHmVtgG1hAEajMg",
@@ -15,17 +17,39 @@ class App extends React.Component {
             storageBucket: "authentication-d4fdd.appspot.com",
             messagingSenderId: "657493032311"
         })
+
+        firebase.auth().onAuthStateChanged(user => {
+            if (user) {
+                this.setState({loggedIn: true})
+            } else {
+                this.setState({loggedIn: false})
+            }
+        })
     }
-    
-    
-    render() {
-        return (
-            <View>
-                <Header headerText="Authentication" />
-                <LoginForm />
-            </View>
-        ) 
-    }
-}
+
+    renderContent() {
+        switch (this.state.loggedIn) {
+            case true:
+                return ( 
+                    <Button onPress={() => firebase.auth().signOut()}>
+                        Log Out 
+                    </Button>
+                );
+            case false:
+                return <LoginForm />;
+            default:
+                return <Spinner size = "large" / > ;
+      }
+  }
+
+  render() {
+        return ( 
+            <View >
+                <Header headerText = "Authentication" /> 
+                {this.renderContent()} 
+          </View>
+      );
+  }
+  }
 
 export default App;
--- auth/src/components/common/Button.js
+++ auth/src/components/common/Button.js
@@ -5,8 +5,8 @@ const Button = ({onPress, children}) => {
     const {buttonStyle, textStyle} = styles;
 
     return ( 
-        <TouchableOpacity style = {buttonStyle} onPress = {onPress}>
-            <Text style={textStyle}>{children} </Text> 
+        <TouchableOpacity style={buttonStyle} onPress={onPress}>
+            <Text style={textStyle}>{children}</Text> 
         </TouchableOpacity>
     );
 };
@@ -20,18 +20,17 @@ const styles = {
         fontWeight: '600',
         paddingTop: 10,
         paddingBottom: 10
-
     },
     buttonStyle: {
         flex: 1,
         alignSelf: 'stretch',
-        backgroundColor: "#fff",
+        backgroundColor: '#fff',
         borderRadius: 5,
         borderWidth: 1,
         borderColor: '#007aff',
         marginLeft: 5,
-        marginRight: 5,
+        marginRight: 5
     }
-}
+};
 
 export { Button }
